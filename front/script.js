0be8160fe980d1dc1fbaa41a99620f87a97fd797
derldalfor100 <derldalfor100@gmail.com>
2019-03-30 20:20:00 +0300

    localstorage funcs

--- front/script.js
+++ front/script.js
@@ -12,3 +12,27 @@ function connect() {
     }).then(response => response.json()).then(data => console.log('The data is:', data))
         .catch(err => console.error('The error:', err))
 }
+
+function sendMessage() {
+    let name = document.getElementById('name').value;
+    let selector = document.getElementById('selector');
+    let selected = selector.options[selector.selectedIndex].value;
+    let message = document.getElementById('message').value;
+    if(selected === 'verbose' || selected === 'fatal') {
+
+    }else {
+
+    }
+}
+
+function addMessage(obj) {
+    let item = localStorage.setItem('messages', JSON.stringify(obj));
+}
+
+function getMessages() {
+    let item = localStorage.getItem('messages');
+    if(item !== null){
+        return JSON.parse(item);
+    }
+    return [];
+}
